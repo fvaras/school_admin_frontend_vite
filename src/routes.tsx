8f6191c21cf6b66e9dc0a733ab80cc28e529bf45
Fernando Varas <fdovarasc@gmail.com>
2024-08-10 00:33:09 -0400

    default SignIn route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from "react"
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import PrivateRoute from "./components/navigation/PrivateRoute";
 import { ADMINISTRATOR_PROFILE_ID, TEACHER_PROFILE_ID } from "./constants/profile";
 
@@ -10,37 +10,36 @@ const TeacherRoutes = lazy(() => import("./features/teacher/routes"));
 const MainRoutes = () => {
   return (
     <Routes>
-      <Route path="/">
-        <Route
-          path="auth/*"
-          element={
-            <Suspense fallback={<div>Loading Auth Routes...</div>}>
-              <AuthRoutes />
-            </Suspense>
-          }
-        />
-        <Route
-          path="admin/*"
-          element={
-            <Suspense fallback={<div>Loading Admin Routes...</div>}>
-              <PrivateRoute allowedProfiles={[ADMINISTRATOR_PROFILE_ID]}>
-                <AdminRoutes />
-              </PrivateRoute>
-            </Suspense>
-          }
-        />
-        <Route
-          path="teacher/*"
-          element={
-            <Suspense fallback={<div>Loading Teacher Routes...</div>}>
-              {/* TODO: Delete ADMINISTRATOR_PROFILE_ID */}
-              <PrivateRoute allowedProfiles={[TEACHER_PROFILE_ID, ADMINISTRATOR_PROFILE_ID]}>
-                <TeacherRoutes />
-              </PrivateRoute>
-            </Suspense>
-          }
-        />
-      </Route>
+      <Route path="/" element={<Navigate to="/auth/signin" replace />} />
+      <Route
+        path="auth/*"
+        element={
+          <Suspense fallback={<div>Loading Auth Routes...</div>}>
+            <AuthRoutes />
+          </Suspense>
+        }
+      />
+      <Route
+        path="admin/*"
+        element={
+          <Suspense fallback={<div>Loading Admin Routes...</div>}>
+            <PrivateRoute allowedProfiles={[ADMINISTRATOR_PROFILE_ID]}>
+              <AdminRoutes />
+            </PrivateRoute>
+          </Suspense>
+        }
+      />
+      <Route
+        path="teacher/*"
+        element={
+          <Suspense fallback={<div>Loading Teacher Routes...</div>}>
+            {/* TODO: Delete ADMINISTRATOR_PROFILE_ID */}
+            <PrivateRoute allowedProfiles={[TEACHER_PROFILE_ID, ADMINISTRATOR_PROFILE_ID]}>
+              <TeacherRoutes />
+            </PrivateRoute>
+          </Suspense>
+        }
+      />
     </Routes>
   );
 };
