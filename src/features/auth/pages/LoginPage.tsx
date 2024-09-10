502e1f3ac1a8aa0df631e010b9a3d48bfa5f2907
Fernando Varas <fdovarasc@gmail.com>
2024-09-10 02:36:46 -0300

    Login image

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -115,7 +115,14 @@ const Signin = () => {
     <div className="auth-container">
       <div
         className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex" style={{ backgroundImage: 'url(assets/images/pages/logo 2.webp)' }}>
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex"
+          style={{
+            backgroundImage: "url('/assets/images/front_1.jpg')",
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'contain'
+          }}
+        >
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
